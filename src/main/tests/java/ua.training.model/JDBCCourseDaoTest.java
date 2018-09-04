65056d709d225ec3f7afe5cf556538d9c59f51d6
RybkinVladyslav <Footballer0@mail.ru>
2018-09-04 06:06:45 +0300

    fix

---
+++ src/main/tests/java/ua.training.model/JDBCCourseDaoTest.java
@@ -0,0 +1,35 @@
+package java.ua.training.model;
+
+import static org.junit.Assert.*;
+
+public class JDBCCourseDaoTest {
+
+    @org.junit.Test
+    public void create() {
+
+    }
+
+    @org.junit.Test
+    public void findById() {
+    }
+
+    @org.junit.Test
+    public void findAll() {
+    }
+
+    @org.junit.Test
+    public void update() {
+    }
+
+    @org.junit.Test
+    public void delete() {
+    }
+
+    @org.junit.Test
+    public void close() {
+    }
+
+    @org.junit.Test
+    public void insertStudentCourses() {
+    }
+}
